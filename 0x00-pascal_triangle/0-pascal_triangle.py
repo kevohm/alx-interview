f6e5ce825a8adabed72e43b7a598ffd829d50ab1
kevohm <kevinkbet@gmail.com>
2022-12-21 08:58:57 +0000

    Finished pascals triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Calculate the pascals's triangle
+"""
+import math
+
+def factorial(n):
+    """
+    find factorial
+    """
+    if n <= 1:
+        return 1
+    return n * factorial(n-1)
+
+def combination(n,r):
+    """
+    calculating nCr
+    """
+    top = factorial(n)
+    bottom = factorial(r)
+    bottom = bottom * factorial(n-r)
+    return math.floor(top / bottom)
+
+def getPascalRow(n):
+    """
+    Pascals values for n
+    """
+    arr = []
+    for i in range(0,n+1):
+        arr.append(combination(n,i))
+    return arr
+
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    arr = []
+    if n < 1:
+        return []
+    for i in range(0,n):
+        arr.append(getPascalRow(i))
+    return arr
