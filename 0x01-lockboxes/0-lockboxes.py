78efb471ae7a9991b5a7bde6a22a12a382fbc9cd
kevohm <kevinkbet@gmail.com>
2023-01-11 10:47:32 +0000

    finished

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+""" oppening boxes"""
 
 def getKeys(boxes):
     """get all keys"""
---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""calculates the fewest number of operations needed to result 
+in exactly n H characters in the file"""
+
+def minOperations(n):
+    data = "H"
+    copy = ""
+    for i in range(1, n+1):
+        if len(data) == n:
+            return i
+        if len(copy) + len(data) == n:
+            return i
+        elif i%3 == 1:
+            copy = data
+        else:
+            if len(copy) != 0:
+                data = copy + data
+    print(data)
