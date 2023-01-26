5403c74a11684655ee3602c213099d93c1b7b598
kevohm <kevinkbet@gmail.com>
2023-01-26 16:17:20 +0000

    fixed_input_error

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,6 +6,8 @@
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding
     """
+    if not isinstance(data, list) or data is None:
+        return False
     for i in data:
         try:
             i.to_bytes(2, "big").decode("utf-8")
