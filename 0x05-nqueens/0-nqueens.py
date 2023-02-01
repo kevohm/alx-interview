5251bf5cf92493bfe65b372fd25268702a0936f7
kevohm <kevinkbet@gmail.com>
2023-02-01 08:42:39 +0000

    finished task-0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,7 +1,31 @@
 #!/usr/bin/python3
 """0-nqueens
 """
-import sys
+from sys import exit, argv
+
+
+def main():
+    """entry point
+    """
+    args = argv
+    if len(args) == 2:
+        data = args[1]
+        try:
+            data = int(data)
+        except ValueError:
+            print("N must be a number")
+            exit(1)
+
+        if data < 4:
+            print("N must be at least 4")
+            exit(1)
+        instance = Queen(data)
+        ans = instance.frame_ans()
+        for i in ans:
+            print(i)
+    else:
+        print("Usage: nqueens N")
+        exit(1)
 
 
 class Queen():
@@ -10,12 +34,6 @@ class Queen():
     def __init__(self, n):
         """initialize object
         """
-        if not isinstance(n, int):
-            print("N must be a number")
-            sys.exit(1)
-        if n < 4:
-            print("N must be at least 4")
-            sys.exit(1)
         self.store = []
         self.n = n
         self.state = []
@@ -45,14 +63,30 @@ class Queen():
         """
         if self.check_validity():
             self.store.append(self.state.copy())
-            return 
+            return
         for i in self.find_all():
             self.state.append(i)
             self.get_safe_areas()
             self.state.pop()
+
     def get_ans(self):
         """return answer
         """
         self.get_safe_areas()
         return self.store
 
+    def frame_ans(self):
+        """convert to required ans
+        """
+        ans = self.get_ans()
+        final = []
+        for k, v in enumerate(ans):
+            art = []
+            for i, j in enumerate(v):
+                art.append([i, j])
+            final.append(art)
+        return final
+
+
+if __name__ == "__main__":
+    main()
