405509548c0d065529ac98e5edc6dd59ca0364e7
Kevinkibet <58782210+kevohm@users.noreply.github.com>
2023-02-10 18:35:32 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-//prints all characters of a Star Wars movie
-const request = require("request")
-request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function(error, response, body){
-if(error){
-	console.log(error)
-}else{	
-	JSON.parse(body).characters.forEach(
-		(item)=>{
-			request(item, function(error, response, body){
-				if(!error){
-					console.log(JSON.parse(body).name)
-				}
-			})
-		}
+const request = require('request');
 
-	)
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
+  }
+});
+
+function printCharacters (characters, index) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
 }
-})
-
