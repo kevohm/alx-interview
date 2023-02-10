2a198c06bf57bf708301987253b60d1ad12fa7f3
kevohm <kevinkbet@gmail.com>
2023-02-10 06:51:41 -0800

    finished taask

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+//prints all characters of a Star Wars movie
+const request = require("request")
+request(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, function(error, response, body){
+if(error){
+	console.log(error)
+}else{	
+	JSON.parse(body).characters.forEach(
+		(item)=>{
+			request(item, function(error, response, body){
+				if(error){console.log(error)}else{
+					console.log(JSON.parse(body).name)
+				}
+			})
+		}
+
+	)
+}
+})
+
